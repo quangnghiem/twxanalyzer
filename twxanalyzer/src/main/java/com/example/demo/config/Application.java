e62678247a60e25422492612ef92740acb087c5c
QuangNghiem <quangnghiemuit@gmail.com>
2018-12-12 16:50:49 +0700

    start project

---
+++ twxanalyzer/src/main/java/com/example/demo/config/Application.java
@@ -0,0 +1,29 @@
+package com.example.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.demo")
+@Import({BeanConfig.class, WebConfig.class})
+//@EnableWebMvc
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
